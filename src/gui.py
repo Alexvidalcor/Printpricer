066e33a4b2e1e10dd8400f06287ff7269dee77da
Alexvidalcor <correovidal@outlook.com>
2021-03-12 12:20:15 +0100

    Finished Personal Costs

--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from src.calc import FilamentCost, KwCost
 from src.handler import MainValidation
 
-sg.SetOptions(element_padding=((50, 0), (10, 10)))
+sg.SetOptions(element_padding=((40, 0), (10, 10)))
 
 
 def MainGui():
@@ -14,39 +14,38 @@ def MainGui():
     layout1 = [[sg.Text('Introduce los siguientes datos para recibir la estimación:', pad=(
         (10, 0), (10, 0)), size=(50, 2), font=(commonParams[2]))],
         [sg.Text('· Precio Kw/hora', size=commonParams[0]),
-         sg.Input(key='-INaccess1-', size=commonParams[1])],
+         sg.Input(key='-INaccess11-', size=commonParams[1])],
         [sg.Text('· Horas de impresión', size=commonParams[0]),
-         sg.Input(key='-INaccess2-', size=commonParams[1])],
+         sg.Input(key='-INaccess21-', size=commonParams[1])],
         [sg.Text('· Coste de bobina', size=commonParams[0]),
-         sg.Input(key='-INaccess3-', size=commonParams[1])],
+         sg.Input(key='-INaccess31-', size=commonParams[1])],
         [sg.Text('· Gramos de bobina completa', size=commonParams[0]),
-         sg.Input(key='-INaccess4-', size=commonParams[1])],
+         sg.Input(key='-INaccess41-', size=commonParams[1])],
         [sg.Text('· Gramos de material consumido', size=commonParams[0]),
-         sg.Input(key='-INaccess5-', size=commonParams[1])],
+         sg.Input(key='-INaccess51-', size=commonParams[1])],
         [sg.Text('· Coste de diseño (Opcional)', size=commonParams[0]),
-         sg.Input(key='-INaccess6-', size=commonParams[1])],
-        [sg.Button("Calcular", font=(commonParams[2]), key="-INsubmit-", auto_size_button=True, pad=((145, 10),
-                                                                                                     (25, 20))), sg.Cancel("Cancelar", key="Quit", font=(commonParams[2]), pad=((0, 0), (25, 20)))]]
+         sg.Input(key='-INaccess61-', size=commonParams[1])],
+        [sg.Button("Calcular", font=(commonParams[2]), key="-INsubmit1-", auto_size_button=True, pad=((115, 10),
+                                                                                                     (25, 20))), sg.Cancel("Cancelar", key="-Quit1-", font=(commonParams[2]), pad=((0, 0), (25, 20)))]]
 
     layout2 = [[sg.Text('Introduce los siguientes datos para recibir la estimación:', pad=(
         (10, 0), (10, 0)), size=(50, 2), font=(commonParams[2]))],
         [sg.Text('· Precio Kw/hora', size=commonParams[0]),
-         sg.Input(key='-INaccess1-', size=commonParams[1])],
+         sg.Input(key='-INaccess12-', size=commonParams[1])],
         [sg.Text('· Horas de impresión', size=commonParams[0]),
-         sg.Input(key='-INaccess2-', size=commonParams[1])],
+         sg.Input(key='-INaccess22-', size=commonParams[1])],
         [sg.Text('· Coste de bobina', size=commonParams[0]),
-         sg.Input(key='-INaccess3-', size=commonParams[1])],
+         sg.Input(key='-INaccess32-', size=commonParams[1])],
         [sg.Text('· Gramos de bobina completa', size=commonParams[0]),
-         sg.Input(key='-INaccess4-', size=commonParams[1])],
+         sg.Input(key='-INaccess42-', size=commonParams[1])],
         [sg.Text('· Gramos de material consumido', size=commonParams[0]),
-         sg.Input(key='-INaccess5-', size=commonParams[1])],
+         sg.Input(key='-INaccess52-', size=commonParams[1])],
         [sg.Text('· Coste de diseño (Opcional)', size=commonParams[0]),
-         sg.Input(key='-INaccess6-', size=commonParams[1])],
-        [sg.Button("Calcular", font=(commonParams[2]), key="-INsubmit-", auto_size_button=True, pad=((145, 10),
-                                                                                                     (25, 20))), sg.Cancel("Cancelar", key="Quit", font=(commonParams[2]), pad=((0, 0), (25, 20)))],
+         sg.Input(key='-INaccess62-', size=commonParams[1])],
         [sg.HorizontalSeparator()],
-        [sg.Text(
-            f"El coste total es {electricityCost+materialCost}", font=(commonParams[2]))]
+        [sg.Text(f"El coste total es {electricityCost+materialCost}", font=(commonParams[2]))],
+        [sg.Button("Calcular", font=(commonParams[2]), key="-INsubmit2-", auto_size_button=True, pad=((115, 10),
+                                                                                                     (25, 20))), sg.Cancel("Cancelar", key="-Quit2-", font=(commonParams[2]), pad=((0, 0), (25, 20)))],
     ]
 
     layoutMain = [[sg.Column(layout1, key='-COL1-'),
@@ -57,15 +56,15 @@ def MainGui():
     layout = 1
     while True:
         event, values = window.read()
-        if event == "-INsubmit-":
+        if event == f"-INsubmit{layout}-":
             window[f'-COL{layout}-'].update(visible=False)
             electricityCost = KwCost(MainValidation(
-                values["-INaccess1-"]), MainValidation(values["-INaccess2-"]))
-            materialCost = FilamentCost(MainValidation(values["-INaccess3-"]), MainValidation(
-                values["-INaccess4-"]), MainValidation(values["-INaccess5-"]))
+                values[f"-INaccess1{layout}-"]), MainValidation(values[f"-INaccess2{layout}-"]))
+            materialCost = FilamentCost(MainValidation(values[f"-INaccess3{layout}-"]), MainValidation(
+                values[f"-INaccess4{layout}-"]), MainValidation(values[f"-INaccess5{layout}-"]))
             layout = 1 if layout == 2 else 2
             window[f'-COL{layout}-'].update(visible=True)
-        if event == sg.WIN_CLOSED or event == 'Quit':
+        if event == sg.WIN_CLOSED or event == f'-Quit{layout}-':
             break
 
     window.close()
